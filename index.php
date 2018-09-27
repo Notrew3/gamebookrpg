7cdb8c3416e4c25b76af64f2e5587c8436dfa674
Ewerton Azevedo <ewerton.azevedo@hotmail.com>
2018-09-27 19:09:04 -0300

    Login e Session

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-
+session_start();
 require_once("vendor/autoload.php");
 
 use  \Slim\Slim;
 use \Game\DB\Sql;
 use \Game\PageAdmin;
 use \Game\Page;
+use \Game\Models\User;
 
 
 $app = new Slim();
@@ -30,11 +31,43 @@ $app->get('/livro', function(){
 
 $app->get('/painel', function(){
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
 }); 
+
+$app->get('/painel/login', function(){
+
+	User::isLogged();
+
+	$page = new PageAdmin(array(
+		"header"=>false,
+		"footer"=>false
+	));
+
+	$page->setTpl("login");
+
+}); 
+
+$app->post('/painel/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /painel");
+	exit;
+
+}); 
+
+$app->get('/painel/logout', function(){
+
+	User::logout();
+
+	header("Location: /painel/login");
+	exit;
+});
 
 $app->run();
 
---
+++ vendor/gamebook/classes/src/Models/User.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Game\Models;
+
+use \Game\DB\Sql;
+use \Game\Model;
+
+class User extends Model
+{
+
+	const SESSION = "User";
+
+	public static function login($login, $password)
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM user WHERE login = :LOGIN", array(
+			":LOGIN"=>$login
+		));
+
+		if(count($results) === 0)
+		{
+			throw new \Exception("Usuário ou senha inválida");			
+		}
+
+		$data = $results[0];
+
+		if(password_verify($password, $data["password"]) === true)
+		{
+			$user = new User();
+
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
+
+		}else{
+
+			throw new \Exception("Usuário ou senha inválida");
+		}
+	}
+
+	public static function verifyLogin()
+	{
+		if(
+			!isset($_SESSION[User::SESSION])
+			||
+			!$_SESSION[User::SESSION]
+			||
+			!(int)$_SESSION[User::SESSION]["id"] > 0
+			
+		){
+
+			header("Location: /painel/login");
+			exit;
+		}
+	}
+
+	public static function isLogged()
+	{
+		if(isset($_SESSION[User::SESSION]))
+		{
+			header("Location: /painel");
+			exit;
+		}
+	}
+
+	public static function logout()
+	{
+		$_SESSION[User::SESSION] = NULL;
+		
+	}
+}
+
+?>
